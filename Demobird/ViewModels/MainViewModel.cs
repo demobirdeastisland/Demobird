38f2d809bd2b1f8e17b770ba8c9392d32a31a508
demobird <demobirdeastisland@gmail.com>
2019-05-15 17:35:09 +0900

    Delete Unnecessary File

--- Demobird/ViewModels/MainViewModel.cs
+++ Demobird/ViewModels/MainViewModel.cs
@@ -8,14 +8,12 @@ namespace Demobird.ViewModels
         public MainViewModel()
         {
         }
-        
+
         public override Task Initialize()
         {
-            //TODO: Add starting logic here
-		    
             return base.Initialize();
         }
-        
+
         public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
         private void ResetText()
         {
