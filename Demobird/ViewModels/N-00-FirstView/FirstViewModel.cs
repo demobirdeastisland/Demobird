5ffe7c1c4f1e4baa22d8d5873c6102b501b35cf3
demobird <demobirdeastisland@gmail.com>
2019-05-15 16:56:14 +0900

    Add ViewModel

---
+++ Demobird/ViewModels/N-00-FirstView/FirstViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace Demobird.ViewModels.N00FirstView
+{
+    public class FirstViewModel : MvxViewModel
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return this._firstName; }
+            set { this._firstName = value; RaisePropertyChanged(() => FullName); }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return this._lastName; }
+            set { this._lastName = value; RaisePropertyChanged(() => FullName); }
+        }
+
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", _firstName, _lastName); }
+        }
+    }
+}
