425496a874418a6f20062044652eca075a5353ad
demobird <demobirdeastisland@gmail.com>
2019-05-16 10:29:13 +0900

    Develop using xib application

---
+++ iOS/Views/N-00-FirstView/FirstView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MvvmCross.iOS.Views;
+using MvvmCross.Binding.BindingContext;
+using UIKit;
+using Demobird.ViewModels.N00FirstView;
+
+namespace Demobird.iOS.Views.N00FirstView
+{
+    public partial class FirstView : MvxViewController
+    {
+        public FirstView() : base("FirstView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+            set.Bind(this.textFieldFirstName).To(vm => vm.FirstName);
+            set.Bind(this.textFieldLastName).To(vm => vm.LastName);
+            set.Bind(this.labelFullName).To(vm => vm.FullName);
+            set.Apply();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ iOS/Views/N-00-FirstView/FirstView.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Demobird.iOS.Views.N00FirstView
+{
+    [Register ("FirstView")]
+    partial class FirstView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelFullName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textFieldFirstName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textFieldLastName { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (labelFullName != null) {
+                labelFullName.Dispose ();
+                labelFullName = null;
+            }
+
+            if (textFieldFirstName != null) {
+                textFieldFirstName.Dispose ();
+                textFieldFirstName = null;
+            }
+
+            if (textFieldLastName != null) {
+                textFieldLastName.Dispose ();
+                textFieldLastName = null;
+            }
+        }
+    }
+}
